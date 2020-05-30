015c51675ed622aae2cc666a34fd167de22867da
Minfante <Minfante377@bitbucket.org>
2020-05-30 15:37:49 -0300

    Add plot from currency price in the last 4hs

--- telegram.py
+++ telegram.py
@@ -2,6 +2,11 @@ import json
 import requests
 import os
 from database import check_user, add_user, set_field, reset_field, get_state, change_ths
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+
+HOURS = 4
 
 class telegram_bot():
     
@@ -29,6 +34,14 @@ class telegram_bot():
         url = self.url + "sendMessage?text={}&chat_id={}".format(message,number)
         self.get_url(url)
     
+    def send_img(self,number,img_path):
+        print("Sending file...")
+        img = open(img_path,"rb")
+        url = self.url + "sendPhoto?chat_id={}".format(number)
+        files = {'photo':img}
+        response = requests.post(url, files = files)
+        print(response)
+
     def check_new_msg(self):
         url = self.url + "getUpdates?offset={}".format(self.offset)
         content = self.get_url(url)
@@ -52,13 +65,19 @@ class telegram_bot():
                         self.send_message(user_id, "Hubo un problema registrando sus valores. Intentelo nuevamente")
                     reset_field(user_id, "on_register")
                 else:
-                    if(txt == '1'):
+                    print(txt)
+                    if txt == '1':
                         history_file = open("./history.txt")
                         last_prices = history_file.readlines()[-1]
                         history_file.close()
                         last_prices = last_prices.strip("\n")
                         last_prices = last_prices.split(",")
                         self.send_message(user_id,"Los ultimos precios registrados son:\nFECHA: "+last_prices[0]+"\nBTC = "+last_prices[1]+" USD"+"\nETH = "+last_prices[2]+" USD")
+                    elif txt == '2':
+                        generate_plot('BTC', HOURS)
+                        generate_plot('ETH', HOURS)
+                        self.send_img(user_id,"./btc.png")
+                        self.send_img(user_id,"./eth.png")
                     elif txt == '3':
                         self.send_message(user_id,"Para registrarse correctamente, escriba los limites superiores e inferiores de su alarma separados por coma. Por ejemplo: btc_sup,btc_inf,eth_sup,eth_inf.\n")
                         set_field(user_id, "on_register")
@@ -95,3 +114,44 @@ def get_str(file_path):
     for line in file.readlines():
         text = text+line
     return text
+
+def generate_plot(name,hours):
+    dates = []
+    btc = []
+    eth = []
+    n = 0
+    history_file = open('./history.txt','r')
+    lines  = history_file.readlines()
+    history_file.close()
+    date = datetime.now()
+    while (datetime.now() - date).total_seconds() < hours * 3600:
+        n = n + 1
+        line = lines[-n].split(",")
+        date = datetime.strptime(line[0], "%Y-%m-%d %H:%M:%S.%f")
+        btc_price = float(line[1])
+        eth_price = float(line[2])
+        dates.append(date)
+        btc.append(btc_price)
+        eth.append(eth_price)
+    if name == 'BTC':
+        fig, ax = plt.subplots()
+        ax.plot(dates,btc)
+        ax.set_title("Evolution of the price in the last "+str(hours)+" h")
+        plt.xlabel("Date")
+        plt.ylabel("ETH price")
+        fig.autofmt_xdate()
+        ax.fmt_xdata = mdates.DateFormatter('%H:%M') 
+        plt.grid()
+        plt.savefig("./btc.png")
+        plt.clf()
+    if name == 'ETH':
+        fig, ax = plt.subplots()
+        ax.plot(dates,eth)
+        ax.set_title("Evolution of the price in the last "+str(hours)+" h")
+        plt.xlabel("Date")
+        plt.ylabel("ETH price")
+        fig.autofmt_xdate()
+        ax.fmt_xdata = mdates.DateFormatter('%H:%M') 
+        plt.grid()
+        plt.savefig("./eth.png")
+        plt.clf()
